c560487f270a3cebccdedd4fd533d7386373e15b
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2023-03-22 00:47:47 +0100

    optimize code

--- assets/js/pi.js
+++ assets/js/pi.js
@@ -1,29 +1,26 @@
-function getRandomPiDigit() {
+async function getRandomPiDigit() {
   const position = Math.floor(Math.random() * 1000000);
   let currentValue = 0;
   const result = document.getElementById("result");
-  const xhttp = new XMLHttpRequest();
+  const positionEl = document.getElementById("position");
+  const interval = 100; // Increase this value to slow down the animation
 
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          const pi = this.responseText;
-          const piDigit = pi.charAt(position);
-          const timer = setInterval(()=>{
-              currentValue += 1;
-              result.innerText = currentValue;
-              if (currentValue >= piDigit) {
-                  clearInterval(timer);
-                  result.innerText = piDigit;
-              }
-          }
-          , 50);
-          document.getElementById("position").innerHTML = `at the position ${position+1}.`;
-          if (!document.getElementById("position").classList.contains("fade-in")) {
-              document.getElementById("position").classList.add("fade-in");
-          }
-      }
+  const response = await fetch("pi_digits.txt");
+  const pi = await response.text();
+  const piDigit = pi.charAt(position);
+
+  function updateResult(timestamp) {
+    currentValue = Math.min(currentValue + 1, piDigit);
+    result.innerText = currentValue;
+    if (currentValue < piDigit) {
+      setTimeout(() => {
+        requestAnimationFrame(updateResult);
+      }, interval);
+    } else {
+      positionEl.innerHTML = `at the position ${position+1}.`;
+      positionEl?.classList?.add("fade-in");
+    }
   }
-  ;
-  xhttp.open("GET", "pi_digits.txt", true);
-  xhttp.send();
+
+  requestAnimationFrame(updateResult);
 }
